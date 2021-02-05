147d6707fc649c4a07626bb391271ef984bfa224
Xinyi Li <10118462+li-xin-yi@users.noreply.github.com>
2021-02-04 23:40:22 -0500

    Test if `eif` file exists
    Before removing the output from the last building,  the script should check if the file exists, or it may report an error in cold-starting.

--- http-proxy/server/build.sh
+++ http-proxy/server/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-rm nitro-enclave-python-demo.eif
+FILE=nitro-enclave-python-demo.eif
+if [ -f "$FILE" ]; then
+    rm $FILE
+fi
 docker rmi -f $(docker images -a -q)
 nitro-cli build-enclave --docker-dir ./ --docker-uri richardfan1126/nitro-enclave-python-demo:latest --output-file nitro-enclave-python-demo.eif
 nitro-cli run-enclave --cpu-count 2 --memory 2048 --eif-path nitro-enclave-python-demo.eif --debug-mode
